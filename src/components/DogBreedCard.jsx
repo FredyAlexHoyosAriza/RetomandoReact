3918bac7ab54450ad1140d1c9eaacfc2a0f66d9f
FredyAlexHoyosAriza <donotgobehind@gmail.com>
2024-09-06 19:24:52 -0500

    Se elimina un input en el componente DogBreedCard

--- src/components/DogBreedCard.jsx
+++ src/components/DogBreedCard.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const DogBreedCard = ({ Breed, DogImg, DogImgAltMsg, likes1, comments, views }) => {
+const DogBreedCard = ({ Breed, DogImg, likes, comments, views }) => {
     let page;
     if (Breed === 'Pastor Belga Malinois') {
         page = '/pastor_belga';
@@ -14,7 +14,7 @@ const DogBreedCard = ({ Breed, DogImg, DogImgAltMsg, likes1, comments, views }) 
                 <div>
                     <label>{Breed}</label>
                     <ul className="interaction">
-                        <li><i className="fa fa-heart"></i><span>{likes1}</span></li>
+                        <li><i className="fa fa-heart"></i><span>{likes}</span></li>
                         <li><i className="fa fa-comment"></i><span>{comments}</span></li>
                         <li><i className="fa fa-eye"></i><span>{views}</span></li>
                     </ul>
